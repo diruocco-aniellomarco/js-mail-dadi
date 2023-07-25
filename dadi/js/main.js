8fb9a72cfd7ab327dc00a336c64e5d659916cc2c
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-25 09:29:58 +0200

    Fix pareggio

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,15 +1,34 @@
 const risultatoSfida = document.getElementById('risultato');
 const button = document.getElementById('button')
+const userPoint = document.getElementById('userpoint');
+const pcPoint = document.getElementById('pcpoint');
+
 
 
 button.addEventListener('click', function() {
     const user = Math.floor(Math.random() * 6) + 1;
 
-const computer = Math.floor(Math.random() * 6) + 1;
+    const computer = Math.floor(Math.random() * 6) + 1;
 
-const risultato = (user > computer) ? 'HAI VINTO' : 'HAI PERSO';
+    let risultato ='';
 
-console.log(risultato);
+    if (user == computer) {
+        risultato ='PAREGGIO';
+    }
+    if (user > computer) {
+        risultato ='HAI VINTO';
+    }
+    if (user < computer) {
+        risultato ='HAI PERSO';
+    } 
 
-risultatoSfida.innerHTML = risultato;
+    // let risultato = 'PAREGGIO'
+    
+    // risultato = (user > computer) ? 'HAI VINTO' : 'HAI PERSO';
+
+    console.log(risultato);
+
+    risultatoSfida.innerHTML = risultato;
+    userPoint.innerHTML = user;
+    pcPoint.innerHTML = computer;
 });
